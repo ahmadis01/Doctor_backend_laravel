ef91bd05194dec6e86651ac2bf17c7a36327f6de
AhmadIsmail <ahmad99net4@gmail.com>
2023-05-20 11:36:35 +0300

    first_commit

---
+++ database/migrations/2023_05_18_091839_create_specializations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecializationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specializations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('Doctor_id')->unsigned();
+            $table->bigInteger('Illness_id')->unsigned();
+            $table->foreign('Doctor_id')->references('id')->on('doctors')->onDelete('cascade');
+            $table->foreign('Illness_id')->references('id')->on('illnesses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specializations');
+    }
+}
